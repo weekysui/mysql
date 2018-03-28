42af5743def54aa3e75a8e6d1196dc9136b705eb
weekysui <weekysui@hotmail.com>
2018-03-27 20:18:35 -0700

    update GROUCHO and MUCHO GROUCHO question

--- MySQL Homework.sql
+++ MySQL Homework.sql
@@ -20,11 +20,11 @@ alter table actor drop column middle_name;
 -- 4a. List the last names of actors, as well as how many actors have that last name.
 select last_name last_name, count(*) num from actor group by last_name;
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
-select last_name last_name, count(*) num from actor group by last_name having num > 2;
+select last_name last_name, count(*) num from actor group by last_name having num > 1;
 -- 4c. Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
 update actor set first_name = 'HARPO' where first_name = 'GROUCHO' and last_name='WILLIAMS';
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO GROUCHO, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! (Hint: update the record using a unique identifier.)
-update actor set first_name = case first_name when 'HARPO' then 'GROUCHO' when 'GROUCHO' then 'MUCHO GROUCHO'  else first_name end;
+update actor set first_name = case first_name  when 'GROUCHO' then 'MUCHO GROUCHO' when 'HARPO' then 'GROUCHO' else first_name end;
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 show create table address;
 show columns from address;
