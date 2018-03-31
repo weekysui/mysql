cb71a0f3c6a0e919f049ddfa8ddccf899e41fae5
weekysui <weekysui@hotmail.com>
2018-03-31 14:00:28 -0700

    added space between first name and last name

--- MySQL Homework.sql
+++ MySQL Homework.sql
@@ -2,7 +2,7 @@ use sakila;
 -- 1a. Display the first and last names of all actors from the table actor.
 select first_name, last_name from actor;
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
-select concat(first_name, last_name) as Actor_name from actor;
+select concat(first_name,' ',last_name) as Actor_name from actor;
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 select actor_id, first_name, last_name from actor where first_name = 'JOE';
 -- 2b. Find all actors whose last name contain the letters GEN:
